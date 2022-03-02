3ffb3bb1ed1aad538a2c8d6d2e41aaec029ebf12
Nick Bruhnev <nicholaspeterb@gmail.com>
2022-03-02 18:04:31 +0200

    version 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,39 +14,52 @@ export class HomeComponent implements OnInit {
   inputFormControl = new FormControl('', [Validators.required]);
   answer: Element;
   result: string = '';
-  success: boolean = false;
+  isDone: boolean = false;
+  guessesLeft: number = 6;
+  guesses: string[] = [];
 
   constructor() {
     this.answer = this.getRandomElement();
+    console.log('answer', this.answer.symbol);
   }
 
   ngOnInit(): void {}
 
   guess() {
     const input = this.inputFormControl.value;
-    const resSymbol = input.charAt(0).toUpperCase() + input.slice(1);
-    const resElem = elements[resSymbol];
-    if (resSymbol in elements) {
-      if (resSymbol === this.answer.symbol) {
-        this.result = 'You got it!';
-        this.success = true;
-      } else {
-        const answerMass: number = Math.round(this.answer.mass);
-        const resMass: number = Math.round(resElem.mass);
-        let massText = '';
-        if (answerMass === resMass) massText = 'Correct';
-        else if (answerMass < resMass) massText = 'Lower';
-        else massText = 'Higher';
+    const guessSym = input.charAt(0).toUpperCase() + input.slice(1);
+    const guessElem = elements[guessSym];
 
-        let catText = '';
-        if (this.answer.category === resElem.category) catText = 'Correct';
-        else catText = 'Wrong';
-
-        this.result = `${massText} Mass, ${catText} Category`;
-      }
-    } else {
-      this.result = 'Invalid Input';
+    if (!(guessSym in elements)) {
+      this.result = 'Invalid input';
+      return null;
     }
+
+    this.guessesLeft -= 1;
+    if (this.guessesLeft === 0) {
+      this.result = 'Sorry, you ran you of guesses!';
+      this.isDone = true;
+      return null;
+    }
+    if (guessSym === this.answer.symbol) {
+      this.result = 'You got it!';
+      this.isDone = true;
+    } else {
+      this.guesses.push(guessSym);
+      const answerMass: number = Math.round(this.answer.mass);
+      const resMass: number = Math.round(guessElem.mass);
+      let massText = '';
+      if (answerMass === resMass) massText = 'Correct';
+      else if (answerMass < resMass) massText = 'Lower';
+      else massText = 'Higher';
+
+      let catText = '';
+      if (this.answer.category === guessElem.category) catText = 'Correct';
+      else catText = 'Wrong';
+
+      this.result = `${massText} Mass, ${catText} Category`;
+    }
+    return null;
   }
 
   tryAgain() {
@@ -59,4 +72,10 @@ export class HomeComponent implements OnInit {
     const elem = elements[symbol];
     return { symbol, ...elem };
   }
+
+  inputClass(num: number) {
+    return `input-${num}`;
+  }
+
+  formatGuessedElems() {}
 }
