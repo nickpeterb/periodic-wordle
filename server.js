50cbbb564f9f336f004c2d7e6cd1172e8a7022dc
Nick Bruhnev <nicholaspeterb@gmail.com>
2022-03-09 17:59:37 +0200

    add ssl redirect

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+import sslRedirect from "heroku-ssl-redirect";
 const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/periodic-wordle"));
+app.use(sslRedirect());
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/periodic-wordle/index.html"));
 });
