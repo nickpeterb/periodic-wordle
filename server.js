6637fcbaabf4c191b6a95c5c4595d37944e3c2d3
Nick Bruhnev <nicholaspeterb@gmail.com>
2022-03-09 18:11:28 +0200

    force ssl

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-import sslRedirect from "heroku-ssl-redirect";
+var forceSsl = require("force-ssl-heroku");
 const path = require("path");
 const app = express();
+
+app.use(forceSsl);
 app.use(express.static(__dirname + "/dist/periodic-wordle"));
-app.use(sslRedirect());
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/periodic-wordle/index.html"));
 });
